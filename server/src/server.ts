09298bdd19bdc204b5b3a681237bd07390a09e43
Leonan Yglecias Mattos <leonanmattos@gmail.com>
2023-05-21 03:45:20 -0300

    aula 4

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,19 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 import { authRouter } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
 import jwt from '@fastify/jwt'
+import { resolve } from 'node:path'
+import multipart from '@fastify/multipart'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -16,6 +26,7 @@ app.register(jwt, {
 })
 app.register(memoriesRoutes)
 app.register(authRouter)
+app.register(uploadRoutes)
 
 app
   .listen({
